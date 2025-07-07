e56820f1bd8b04f512c970f6de9434cdea575c52
vaibhavpan121 <vaibhavpan18@gmail.com>
2025-07-07 19:04:17 +0530

    update

--- app/Views/Home/employee.php
+++ app/Views/Home/employee.php
@@ -2,50 +2,50 @@
     <div class="content-wrapper">
         <!-- Table starts from here  -->
         <div class="row" style="background-color: white;overflow:scroll">
-                <div class="w-100 ele-bg p-4 mb-4 d-flex justify-content-between align-items-center">
-                    <h4 class="mb-0">All Employee List</h4>
-                    <a href="<?= base_url('register') ?>" class="btn custom-button gen-bord">Add Employee</a>
-                </div>
-                <table id="dataTable" class="display table table-hover">
-                    <thead>
+            <div class="w-100 ele-bg p-4 mb-4 d-flex justify-content-between align-items-center">
+                <h4 class="mb-0">All Employee List</h4>
+                <a href="<?= base_url('register') ?>" class="btn custom-button gen-bord">Add Employee</a>
+            </div>
+            <table id="dataTable" class="display table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>User Role</th>
+                        <th>Assign Location</th>
+                        <th>Status</th>
+                        <th>Action</th> <!-- NEW COLUMN -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($users as $user): ?>
                         <tr>
-                            <th>Action</th> <!-- NEW COLUMN -->
-                            <th>ID</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            <th>Address</th>
-                            <th>User Role</th>
-                            <th>Assign Location</th>
-                            <th>Status</th>
+
+                            <td><?= esc($user['user_id']) ?></td>
+                            <td><?= esc($user['name']) ?></td>
+                            <td><?= esc($user['email']) ?></td>
+                            <td><?= esc($user['address']) ?></td>
+                            <td><?= esc($user['user_role']) ?></td>
+                            <td><?= esc($user['assign_location']) ?></td>
+                            <td><?= esc($user['status']) ?></td>
+                            <td class="text-center">
+                                <i class="fa fa-edit fa-2x ele-color c-p user-action-icon"
+                                    data-id="<?= $user['user_id'] ?>"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#userActionModal"></i>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($users as $user): ?>
-                            <tr>
-                                <td class="text-center">
-                                    <i class="fa fa-hand-o-up fa-2x ele-color c-p user-action-icon"
-                                        data-id="<?= $user['user_id'] ?>"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#userActionModal"></i>
-                                </td>
-                                <td><?= esc($user['user_id']) ?></td>
-                                <td><?= esc($user['name']) ?></td>
-                                <td><?= esc($user['email']) ?></td>
-                                <td><?= esc($user['address']) ?></td>
-                                <td><?= esc($user['user_role']) ?></td>
-                                <td><?= esc($user['assign_location']) ?></td>
-                                <td><?= esc($user['status']) ?></td>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
 
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-
-                <div class="pagination-container">
-                    <ul id="pagination" class="pagination">
-                        <!-- Pagination buttons will be dynamically generated here -->
-                    </ul>
-                </div>
+            <div class="pagination-container">
+                <ul id="pagination" class="pagination">
+                    <!-- Pagination buttons will be dynamically generated here -->
+                </ul>
+            </div>
         </div>
 
         <!-- User Action Modal -->
